5296ec123eea9addd0d181bba104b1763ab371c5
Matt Canavan <mattcanavan@users.noreply.github.com>
2021-01-18 14:10:42 -0800

    Updated btn to say 'View Project'.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -41,7 +41,7 @@ export default function Projects() {
                                 target='_blank'
                                 rel='noreferrer'
                                 href={deployedURL}>
-                                    See Live
+                                    View Project
                                 </a>
                             )}
 
