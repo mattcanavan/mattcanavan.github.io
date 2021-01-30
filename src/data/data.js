f23ae40e45be4064e04cc3a8bd800d144bc7608c
Matt Canavan <mattcanavan@users.noreply.github.com>
2021-01-29 21:12:31 -0800

    Added more projects

--- src/data/data.js
+++ src/data/data.js
@@ -10,7 +10,6 @@ export const headData = {
 // ABOUT DATA
 export const aboutData = {
   byline: "Hi, I'm Matt",
-  img: "",
   aboutMe: "I'm passionate about JavaScript and life-long learning.",
   githubURL: "https://github.com/mattcanavan",
   email: "mailto:hello@mattpy.com",
@@ -20,26 +19,64 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv4(),
-    title: "Simple Twitter bot with Python",
-    info:
-      "Python Weekly Report is a great blogspot that collects the ten most popular Python questions asked on StackOverflow the week previous and publishes them all in a single post. I scrape the blog’s RSS feed and tweet each question separately.",
-
-    deployedURL: "https://twitter.com/PopularPythonQs", //if blank, no btn will appear
-    repoURL: "", //if blank, no btn will appear
-    keySkills: ["Python", "Twitter API", "Heroku"],
-
-    img: "twitterBot.png",
-    alt: "example of tweets sent by bot",
+    title: "Vizualize Binary Tree from JSON",
+    info: `JSON can represent a single binary tree node quiet well. 
+    But as the number of nodes increase, the verboseness increases as well and the tree can become difficult to imagine or draw.
+    Sounds like a problem for a computer to solve!`,
+    deployedURL: "https://mattcanavan.dev/visualize-binary-tree-from-json/", //if blank, no btn will appear
+    repoURL: "https://github.com/mattcanavan/visualize-binary-tree-from-json", //if blank, no btn will appear
+    keySkills: ["Data Structures", "Deserialize a JSON String to a Binary Tree", "Measure depth of tree", "Calculate breadth-first traverse"],
+    img: "visualize-binary-tree.png",
+    alt: "binary tree with 15 nodes"
+  },
+  {
+    id: uuidv4(),
+    title: "RESTful API with Express.js, Node and Knex",
+    info: 
+    `Served as the backend of a React App. Users were authenticated with JWT. 
+    SQLite3 was used in development and PostgreSQL in product for data persistence
+    `,
+    deployedURL: "", //if blank, no btn will appear
+    repoURL: "https://github.com/BW-TruckTracker/BackEnd", //if blank, no btn will appear
+    keySkills: ["Express.js", "Knex ORM", "Heroku", "Teamwork (group project)"],
+    img: "backend-node-express.png",
+    alt: "README from github repo"
   },
   {
     id: uuidv4(),
     title: "Portfolio with Gatsby and GitHub Pages",
-    info:
-      "Gatsby is a great resource for generating static websites that can be hosted on GitHub pages for free (and Gatsby utilizes React to create templates/components!)",
+    info: 
+    `The Gatsby framework pairs lightning fast static websites with extremely helpful plugins
+    and the results speak for themselves. Not to mention it's based on React! Seriously check
+    out this framework for your next project.`,
     deployedURL: "https://mattcanavan.dev", //if blank, no btn will appear
     repoURL: "https://github.com/mattcanavan/mattcanavan.github.io", //if blank, no btn will appear
-    keySkills: ["Gatsby", "React", "Context API", "SCSS"],
-    img: "gatsby.jpg",
-    alt: "flowers along a wall. very tasteful",
+    keySkills: ["Gatsby", "React", "GraphQL", "Context API", "SCSS"],
+    img: "gatsby-portfolio-one.png",
+    alt: "homepage of this very website",
+  },
+  {
+    id: uuidv4(),
+    title: "Simple Twitter bot with Python",
+    info:
+    `A bot that tweets the highest rated Python questions asked on StackOverflow the previous week,
+    via scraping the RSS feed of blogspot Python Weekly Report.`,
+    deployedURL: "https://twitter.com/PopularPythonQs", //if blank, no btn will appear
+    repoURL: "", //if blank, no btn will appear
+    keySkills: ["Python", "Twitter API", "Heroku"],
+    img: "twitter-bot.png",
+    alt: "twitter bio picture",
   },
 ]
+
+// template
+// {
+//   id: uuidv4(),
+//   title: "",
+//   info: "",
+//   deployedURL: "", //if blank, no btn will appear
+//   repoURL: "", //if blank, no btn will appear
+//   keySkills: [""],
+//   img: "",
+//   alt: ""
+// },
